28602035fb729b4f4c2cc3319f34c0fe21ee19b9
edinamaljevac <maljevacedina@gmail.com>
2026-03-02 20:34:56 +0100

    Disable auto-checkin scheduler

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('trainings:auto-checkin')
-    ->everyMinute();
+// Schedule::command('trainings:auto-checkin')
+//     ->everyMinute();
